8b7548ba7aec9eb9471c8d1faa3ad02401a6cda4
Dyplopia <valy.bru@outlook.fr>
2022-12-01 18:05:36 +0100

    Essai script meteo

---
+++ Assets/Scripts/meteo.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteo : MonoBehaviour
+{
+   
+
+    //ajouter des particles quand on mettra les assets
+
+    public float chanceOfRain = 50f;
+    public float dice;
+    public bool soleil;
+    public float timer = 0f;
+
+    // Update is called once per frame
+    public void Update()
+    {
+        dice = Random.Range(0f, 100.0f);
+
+        if (dice<chanceOfRain) 
+        {
+            {
+                soleil = true;
+                Debug.Log("Il fait soleil.");
+                timer = Random.Range(5f, 20f);
+            }
+        }
+        
+        if(soleil)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0f) 
+            {
+                soleil = false;
+                Debug.Log("Il pleut.");
+            }
+        }
+    } 
+}
